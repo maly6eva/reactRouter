656f379707f506c42c9e5ae44f8bc9353083f93f
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-05-15 21:44:29 +0300

    Создание компонентов

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "./pages/Home.jsx";
+import {About} from "./pages/About.jsx";
+import {Card} from "./pages/Card.jsx";
+import {Categories} from "./pages/Categories.jsx";
+import {NotFound} from "./pages/NotFound.jsx";
+import {ProductDetails} from "./pages/ProductDetails.jsx";
+
+
+const router = createBrowserRouter([
+    {path: '/', element: <Home/>},
+    {path: 'about', element: <About/>},
+    {path: 'card', element: <Card/>},
+    {path: 'categories', element: <Categories/>},
+    {path: '*', element: <NotFound/>},
+    {path: 'product', element: <ProductDetails/>}
+])
+
 function App() {
-  return <div></div>;
+    return <RouterProvider router={router}/>
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link, NavLink} from "react-router-dom";
 
 export const Home = () => {
     return (
         <div>
+Home Page
+            <div>
+                {/*<a href="http://localhost:5173/about">About</a>*/}
+                <NavLink to={'about'}>About</NavLink>
+                <NavLink to={'card'}>Card</NavLink>
+                <NavLink to={''}>Home</NavLink>
 
+            </div>
         </div>
     );
 };
